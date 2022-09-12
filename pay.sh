deb72fe19462d38eb1f1097aa23fdbcd68cdca7c
Imran Niaz <80633793+imran-niaz@users.noreply.github.com>
2022-09-12 18:50:13 -0400

    Create pay.sh

---
+++ pay.sh
@@ -0,0 +1,16 @@
+
+http://debiandev/sqlmap/mysql/get_int.php?id=1" --batch --tables -D testd                                                
+https://www.grab.com/sg//wp-admin/admin-ajax.php
+https://hackerone.com/imran-niaz?id=1&type=user --batch --tables -D testd .
+https://www.mars.com/sites/g/files/aspx
+ https://o197999.ingest.sentry.io/api/6008007/envelope/?sentry_key=3b4bb33e1dfc432c9ccd9f9ce5a4bd25&sentry_version=7
+ https://login.payoneer.com/gw-auth/api/v2/sms/code
+ https://trackingapi.trendemon.com/api/experience/personal?AccountId=277&ClientUrl=https%3A%2F%2Fwww.payoneer.com%2F&MarketingAutomationCookie=&ExcludeUnitsJson=%5B%5D&callback=jsonp424848&vid=277:16629329627028344
+ https://trackingapi.trendemon.com/api/settings/277?callback=jsonp424848&vid=
+ https://api.userway.org/api/a11y-data/v0/page/https%3A%2F%2Fwww.payoneer.com%2F/DESKTOP/WIDGET_ON/status
+ https://www.payoneer.com/wp-includes/jsp.tgz/
+ https://trackingapi.trendemon.com/api/settings/277?callback=jsonp424848&vid=user+admin
+ https://www.youtube.com/youtubei/v1/log_event?alt=json&key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8
+ https://www.youtube.com/s/desktop/7aa2c37d/jsbin/custom-elements-es5-adapter.vflset/blaze-out/k8-opt/bin/third_party/javascript/custom_elements/fast-shim.js.sourcemap
+ https://www.youtube.com/youtubei/v1/log_event?alt=json&key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8
+ https://www.youtube.com/youtubei/v1/log_event?alt=json&key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8		
