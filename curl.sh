45c904589117876f33033979a34cd3b2f225e95c
Imran Niaz <80633793+imran-niaz@users.noreply.github.com>
2022-09-12 18:28:02 -0400

    Create curl.sh

---
+++ curl.sh
@@ -0,0 +1,41 @@
+
+curl --request POST  --url 
+curl --request GET  --url  
+curl --request PUT  --url  
+curl --request PATCH  --url 
+curl --request DELETE  --url  
+curl --request OPTIONS --url 
+curl --request TRACE --url  
+curl --request HEAD --url  
+curl --request ACL   --url  
+curl --request BASELINE-CONTROL   --url 
+curl --request BIND   --url  
+curl --request CHECKIN	  --url w 
+curl --request RFC3253  --ur 
+curl --request CHECKOUT  --url  
+curl --request RFC3253  --url  
+curl --request COPY  --url  
+curl --request LABEL  --url  
+curl --request LINK  --url  
+curl --request LOCK  --url  
+curl --request MERGE  --url  
+curl --request MKACTIVITY  --url 
+curl --request MKCALENDAR  --url  
+curl --request MKCOL  --url  
+curl --request MKREDIRECTREF  --url  
+curl --request MKWORKSPACE  --url  
+curl --request MOVE  --url 
+curl --request ORDERPATCH  --url 
+curl --request PATCH  --url  
+curl --request RFC5789 --url  
+curl --request PROPFIND   --url  
+curl --request PROPPATCH   --url  
+curl --request REBIND  --url  
+curl --request SEARCH  --url    
+curl --request UNBIND 
+curl --request UNCHECKOUT  
+curl --request UNLINK  
+curl --request UNLOCK  
+curl --request UPDATE  
+curl --request UPDATEREDIRECTREF  
+curl --request  
