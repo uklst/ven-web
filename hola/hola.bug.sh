c177557e84931e7d97b08ca5e721f32da7bd12ae
Imran Niaz <80633793+imran-niaz@users.noreply.github.com>
2022-09-17 16:36:09 -0400

    Create hola.bug.sh

---
+++ hola/hola.bug.sh
@@ -0,0 +1,14 @@
+Recently a small vulnerability that is included in source code easily can be bypass user can use unlimited VPN forever. 
+
+Hola VPN Chrome Ext has a vulnerability that has been exploited by me. I have found the wonder ability by following few steps the vulnerability seems to be small but can affect badly to your system. with this issue the user can use unlimited resources of VPN. 
+
+Vulnerability: 
+
+turn on 	HOLA VPN in Chrome. then he asked for you wait for a time just open inspect mode or open a developer mood for ext the extension will open a frame so you can easily remove the frame and watch unlimited videos the timer will be still running on background but it will not effect on your video streaming 
+
+with the following step you can easily see the vulnerability and the court will be front of you so you can check out 
+
+CODE  
+<iframe style="position: fixed !important; max-width: 100% !important; max-height: 100% !important; background-color: transparent !important; z-index: 2147483647 !important; overflow: hidden !important; visibility: visible !important; border: none !important; display: block !important; inset: auto 15px 0px auto !important; width: 251px !important; height: 96px !important;" src="chrome-extension://gkojfkhlekighikafcpjkiklfbnlmeio/js/popup.html?tab_id=1369412597&amp;connection_id=1369412597:tpopup:1800606875656214" id="_hola_popup_iframe__"></iframe>
+
+ In this caase tight now  timer is active . when time will be over and then you can just remove this frame and enjoy free this area right now  
